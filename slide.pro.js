2b256960ce1f004c6fa1a5c1d82430fcccbc4669
malaviyapunam <159758998+malaviyapunam@users.noreply.github.com>
2024-11-20 13:29:25 +0530

    Add files via upload

---
+++ slide.pro.js
@@ -0,0 +1,71 @@
+let quotes = [
+  {
+      "text":"Life isn’t about getting and having, it’s about giving and being.","author":"Kevin Kruse"
+  },
+  {
+     "text":"Whatever the mind of man can conceive and believe, it can achieve.","author":"Napoleon Hill"
+  },
+  {
+     "text":"Strive not to be a success, but rather to be of value.","author":"Albert Einstein"
+  },
+  {
+     "text":"Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.","author":"Robert Frost"
+  },
+  {
+     "text":"I attribute my success to this: I never gave or took any excuse.","author":"Florence Nightingale"
+  },
+  {
+     "text":"You miss 100% of the shots you don’t take.","author":"Wayne Gretzky"
+  },
+  {
+     "text":"The most difficult thing is the decision to act, the rest is merely tenacity.","author":"Amelia Earhart"
+  },
+  {
+     "text":"Every strike brings me closer to the next home run.","author":"Babe Ruth"
+  },
+  {
+     "text":"Definiteness of purpose is the starting point of all achievement.","author":"W. Clement Stone"
+  },
+  {
+     "text":"We must balance conspicuous consumption with conscious capitalism.","author":"Kevin Kruse"
+  },
+  {
+     "text":"Life is what happens to you while you’re busy making other plans.","author":"John Lennon"
+  },
+  ];
+
+  let index=0;
+
+  function getQuotes()
+  {
+      let text = document.getElementById('quote-text');
+      let author = document.getElementById('quote-author');
+
+      text.innerHTML = ` "${quotes[index].text}" `
+      author.innerHTML = ` - ${quotes[index].author}`
+  }
+
+
+function nextfunction()
+{
+   if (index < quotes.length - 1)
+      {
+         index++;
+         getQuotes();
+      }
+
+}
+function prevfunction()
+{
+   if (index > 0)
+      {
+         index--;
+         getQuotes();
+      }
+
+}
+
+
+
+
+
